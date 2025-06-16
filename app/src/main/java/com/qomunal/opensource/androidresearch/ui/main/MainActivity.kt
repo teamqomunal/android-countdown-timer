9debfa0ae4ff45f583a4e6b5f2f6e952c7d0edb8
amirisback <faisalamircs@gmail.com>
2025-06-16 15:11:39 +0700

    update: countdown timer

--- app/src/main/java/com/qomunal/opensource/androidresearch/ui/main/MainActivity.kt
+++ app/src/main/java/com/qomunal/opensource/androidresearch/ui/main/MainActivity.kt
@@ -1,10 +1,13 @@
 package com.qomunal.opensource.androidresearch.ui.main
 
 import android.os.Bundle
+import android.os.CountDownTimer
 import androidx.activity.viewModels
 import com.qomunal.opensource.androidresearch.common.base.BaseActivity
-import com.qomunal.opensource.androidresearch.common.ext.showToast
 import com.qomunal.opensource.androidresearch.databinding.ActivityMainBinding
+import java.text.DecimalFormat
+import java.text.NumberFormat
+
 
 class MainActivity : BaseActivity<ActivityMainBinding>() {
 
@@ -23,9 +26,21 @@ class MainActivity : BaseActivity<ActivityMainBinding>() {
 
     override fun initUI() {
         binding.apply {
-            btnTest.setOnClickListener {
-                showToast("Yes u click on me")
-            }
+            object : CountDownTimer(50000, 1000) {
+                override fun onTick(millisUntilFinished: Long) {
+                    // Used for formatting digits to be in 2 digits only
+                    val f: NumberFormat = DecimalFormat("00")
+                    val hour = (millisUntilFinished / 3600000) % 24
+                    val min = (millisUntilFinished / 60000) % 60
+                    val sec = (millisUntilFinished / 1000) % 60
+                    btnTest.text = f.format(hour) + ":" + f.format(min) + ":" + f.format(sec)
+                }
+
+                override fun onFinish() {
+                    // When the task is over it will print 00:00:00
+                    btnTest.text = "00:00:00"
+                }
+            }.start()
         }
     }
 
